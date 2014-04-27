ceb413de12645773405748c543207b01c9f0fe87
artur.kink <artur.kink@gmail.com>
2014-04-26 22:45:27 -0400

    Fixed mountain score triggers

--- Source/tojam9/DozerGame.cpp
+++ Source/tojam9/DozerGame.cpp
@@ -13,27 +13,21 @@ ADozerGame::ADozerGame(const class FPostConstructInitializeProperties& PCIP)
 void ADozerGame::BeginPlay(){
 	Super::BeginPlay();
 
-	if (GEngine && GEngine->GetNumGamePlayers(GEngine->GameViewport) == 1){
+	if (GEngine->GetNumGamePlayers(GEngine->GameViewport) == 1){
 		FString Error1;
 
 		//Spawn second player, first player spawned automatically.
 		ULocalPlayer* newPlayer = GEngine->GameViewport->CreatePlayer(1, Error1, true);
 		if (!newPlayer){
-			GEngine->AddOnScreenDebugMessage(-1, 20.0f, FColor::Red, Error1);
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, Error1);
 		}
 		else{
-			GEngine->AddOnScreenDebugMessage(-1, 20.0f, FColor::Red, TEXT("Created Player 2"));
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Created Player 2"));
+			//Move player 2 pawn to start position.
 			newPlayer->PlayerController->StartSpot = FindPlayerStart(NULL, "P2");
-
-			//Move pawn to start position.
-			if (FindPlayerStart(NULL, "P2")){
-				newPlayer->PlayerController->GetPawn()->SetActorLocation(
-					newPlayer->PlayerController->StartSpot->GetActorLocation(), false);
-
-				newPlayer->PlayerController->InitInputSystem();
-				newPlayer->PlayerController->InputComponent->Activate();
-				newPlayer->PlayerController->bBlockInput = false;
-			}
+			newPlayer->PlayerController->GetPawn()->SetActorLocation(
+				newPlayer->PlayerController->StartSpot->GetActorLocation(), false);
 		}
 	}
+	
 }
